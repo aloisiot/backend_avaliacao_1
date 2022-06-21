2e77f47f6d8bdd803dd371e9ff3f76bd946a9421
Aloísio Carvalho <loistc.carvalho@gmail.com>
2022-06-21 20:40:09 -0300

    zipkin e rabbit - implementacoes individuais

--- catalog-service/src/main/java/dh/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/dh/catalogservice/CatalogServiceApplication.java
@@ -2,7 +2,10 @@ package dh.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +13,11 @@ public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
--- movies-service/src/main/java/dh/moviesservice/MoviesServiceApplication.java
+++ movies-service/src/main/java/dh/moviesservice/MoviesServiceApplication.java
@@ -2,12 +2,20 @@ package dh.moviesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MoviesServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
